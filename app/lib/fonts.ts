9a8158a8467ff2bb6d998067d798e7ad60cb1dfc
modern_thanadon <monologyza_za@hotmail.com>
2025-02-14 16:46:57 +0700

    [implement]: font

---
+++ app/lib/fonts.ts
@@ -0,0 +1,8 @@
+import { Castoro } from 'next/font/google';
+
+export const castoro = Castoro({
+    weight: '400',
+    subsets: ['latin'],
+    variable: '--font-castoro',
+    display: 'swap',
+});
