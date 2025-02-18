8821fc9a79776a07cfe3987e20b579c924ba42b4
modern_thanadon <monologyza_za@hotmail.com>
2025-02-18 08:32:13 +0700

    [implement]: CreateButton

---
+++ app/components/common/CreateButton.tsx
@@ -0,0 +1,9 @@
+'use client';
+
+export const CreateButton = () => {
+    return (
+        <button className="bg-success hover:bg-success/90 text-white px-3 py-1.5 rounded text-sm">
+            Create+
+        </button>
+    );
+};
